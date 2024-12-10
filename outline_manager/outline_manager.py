6f6de8979eee5959cad87dabf363f55c04ddf01f
AntonZakhlebayeu <antonzakhlebayeu@gmail.com>
2024-12-10 22:08:08 +0300

    Fixed bug

--- outline_manager/outline_manager.py
+++ outline_manager/outline_manager.py
@@ -15,7 +15,10 @@ class OutlineManager:
     def create_a_new_free_key(self, vpn_type: VPNType, user_id: int, user: User) -> str:
         key_id = db_client.get_key_id(user_id, vpn_type)
         if key_id:
-            self.client.delete_key(key_id)
+            try: 
+                self.client.delete_key(key_id)
+            except Exception as e:
+                print(f"Error deleting key: {e}")
         key = self.client.new(name=str(user_id))
         key.change_data_limit(10000000)
         db_client.add_key_id(user_id, vpn_type, key.id)
@@ -24,5 +27,8 @@ class OutlineManager:
 
     def remove_key(self, user_id: int, vpn_type: VPNType) -> None:
         key_id = db_client.get_key_id(user_id, vpn_type)
-        self.client.delete_key(key_id)
+        try: 
+            self.client.delete_key(key_id)
+        except Exception as e:
+            print(f"Error deleting key: {e}")
         db_client.remove_key_id(vpn_type, user_id)
